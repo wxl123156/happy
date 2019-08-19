fc9e1c4552358db9931ed1052df3414181a720da
LeeHappy <2556558751@qq.com>
2019-08-19 09:16:54 +0800

    0.0

---
+++ src/Member.java
@@ -0,0 +1,33 @@
+public class Member {
+    private  String  name;
+    private  int age;
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
